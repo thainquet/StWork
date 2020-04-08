4ae7b58b03b527ca076090c885a9f6681778f929
thainquet <thainq00@gmail.com>
2020-04-08 11:51:34 +0700

    delete unecessary file

--- navigation/auth-nav.js
+++ navigation/auth-nav.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack'
-import Login from '../Screen/Login'
-import Signup from '../Screen/Signup'
+import Login from '../Screen/authenScreen/Login'
+import Signup from '../Screen/authenScreen/Signup'
+import ForgotPass from '../Screen/authenScreen/ForgotPassword'
 
 const Stack = createStackNavigator()
 const AuthNavigation = () => {
   return (
     <>
-      <Stack.Navigator screenOptions={{headerShown : false }}>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="Signup" component={Signup} options={{title: 'Register new account'}} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPass} options={{title: 'Re-gen my password'}} />
       </Stack.Navigator>
     </>
   )
