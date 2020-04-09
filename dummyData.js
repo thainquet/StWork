b68b49f11898fc0660f112f19ce3360160097c6e
thainquet <thainq00@gmail.com>
2020-04-09 21:25:34 +0700

    add "quận huyện nội thành Hà Nội"

---
+++ dummyData.js
@@ -0,0 +1,84 @@
+const quanNoiThanh = [ {
+    name: "Ba Đình",
+    type: "quan",
+    slug: "ba-dinh",
+    name_with_type: "Quận Ba Đình",
+    path: "Ba Đình, Hà Nội",
+    path_with_type: "Quận Ba Đình, Thành phố Hà Nội",
+    code: "001",
+    parent_code: "01"
+    },{
+    name: "Hoàn Kiếm",
+    type: "quan",
+    slug: "hoan-kiem",
+    name_with_type: "Quận Hoàn Kiếm",
+    path: "Hoàn Kiếm, Hà Nội",
+    path_with_type: "Quận Hoàn Kiếm, Thành phố Hà Nội",
+    code: "002",
+    parent_code: "01"
+    },{
+    name: "Tây Hồ",
+    type: "quan",
+    slug: "tay-ho",
+    name_with_type: "Quận Tây Hồ",
+    path: "Tây Hồ, Hà Nội",
+    path_with_type: "Quận Tây Hồ, Thành phố Hà Nội",
+    code: "003",
+    parent_code: "01"
+    },{
+    name: "Long Biên",
+    type: "quan",
+    slug: "long-bien",
+    name_with_type: "Quận Long Biên",
+    path: "Long Biên, Hà Nội",
+    path_with_type: "Quận Long Biên, Thành phố Hà Nội",
+    code: "004",
+    parent_code: "01"
+    },{
+    name: "Cầu Giấy",
+    type: "quan",
+    slug: "cau-giay",
+    name_with_type: "Quận Cầu Giấy",
+    path: "Cầu Giấy, Hà Nội",
+    path_with_type: "Quận Cầu Giấy, Thành phố Hà Nội",
+    code: "005",
+    parent_code: "01"
+    },{
+    name: "Đống Đa",
+    type: "quan",
+    slug: "dong-da",
+    name_with_type: "Quận Đống Đa",
+    path: "Đống Đa, Hà Nội",
+    path_with_type: "Quận Đống Đa, Thành phố Hà Nội",
+    code: "006",
+    parent_code: "01"
+    },{
+    name: "Hai Bà Trưng",
+    type: "quan",
+    slug: "hai-ba-trung",
+    name_with_type: "Quận Hai Bà Trưng",
+    path: "Hai Bà Trưng, Hà Nội",
+    path_with_type: "Quận Hai Bà Trưng, Thành phố Hà Nội",
+    code: "007",
+    parent_code: "01"
+    }, {
+    name: "Hoàng Mai",
+    type: "quan",
+    slug: "hoang-mai",
+    name_with_type: "Quận Hoàng Mai",
+    path: "Hoàng Mai, Hà Nội",
+    path_with_type: "Quận Hoàng Mai, Thành phố Hà Nội",
+    code: "008",
+    parent_code: "01"
+    },{
+    name: "Thanh Xuân",
+    type: "quan",
+    slug: "thanh-xuan",
+    name_with_type: "Quận Thanh Xuân",
+    path: "Thanh Xuân, Hà Nội",
+    path_with_type: "Quận Thanh Xuân, Thành phố Hà Nội",
+    code: "009",
+    parent_code: "01"
+    },]
+
+export default quanNoiThanh
