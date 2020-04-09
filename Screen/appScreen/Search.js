ff55f7aae2218cff1ec54874a0e0ffac67425729
thainquet <thainq00@gmail.com>
2020-04-09 22:04:57 +0700

    add modal and picker

--- Screen/appScreen/Search.js
+++ Screen/appScreen/Search.js
@@ -1,23 +1,56 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Picker,
+  Modal,
+  TextInput
 } from 'react-native';
 
-const Search =({navigation}) => {
-    return(
-      <>
-        <View>
-          <Text>
-            Homepage
-          </Text>
-          <Button title="Logout"
-          onPress={() => navigation.navigate('AuthNavigation')}/>
-        </View>
-      </>
-    )
-  }
+import quan_huyen from '../../dummyData'
 
-  export default Search
+const URL = 'https://raw.githubusercontent.com/madnh/hanhchinhvn/master/dist/quan_huyen.json'
+
+const Search = ({ navigation }) => {
+  const [quanHuyen, setQuanHuyen] = useState(quan_huyen)
+  const [modalVisible, setModalVisible] = useState(false)
+  const [selectedValue, setSelectedValue] = useState("Tất cả")
+  return (
+    <>
+      <View>
+        <Modal
+          animationType="slide"
+          visible={modalVisible}
+          presentationStyle="fullScreen"
+        >
+          <View style={{ flex: 1 }} >
+
+            <Picker
+              selectedValue={selectedValue}
+              style={{ height: 50, width: 150 }}
+              onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+            >
+              {quanHuyen.map(i => <Picker.Item label={i.name} value={i.name} key="i.code" />)}
+            </Picker>
+            <Button style={{ position: 'absolute', top: 0, left: 0, width: '50%' }} onPress={() => setModalVisible(false)} title="Close" />
+          </View>
+        </Modal>
+        <Text>
+          Homepage
+          </Text>
+        <Text>
+          Homepage
+          </Text>
+        <Text>{selectedValue}</Text>
+        <Button title="Open Modal"
+          // onPress={() => navigation.navigate('AuthNavigation')} />
+          onPress={() => setModalVisible(true)} />
+      </View>
+    </>
+  )
+}
+
+export default Search
