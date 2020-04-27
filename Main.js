30ed89a6f330becdb42fc6914eb0728a3f0f637a
thainquet <thainq00@gmail.com>
2020-04-27 22:15:08 +0700

    add paper package

--- Main.js
+++ Main.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import AppContainer from './navigation'
+import { Provider as PaperProvider } from 'react-native-paper';
 
 const Main = () => {
-  return <AppContainer />
+  return <PaperProvider><AppContainer /></PaperProvider>
 }
 
 export default Main
