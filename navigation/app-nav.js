b27a0be526007d3d51f516266a430ff4ff1e8d89
thainquet <thainq00@gmail.com>
2020-05-28 14:52:57 +0700

    add stack navigator in tab navigator

--- navigation/app-nav.js
+++ navigation/app-nav.js
@@ -12,15 +12,35 @@ import { useNavigation } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createStackNavigator()
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator()
+
+const HomepageDetailStack = createStackNavigator()
+
+const HomeStackScreen = () => {
+  return (
+    <HomepageDetailStack.Navigator initialRouteName="Home">
+      <HomepageDetailStack.Screen name="Home" component={Search} options={{ headerShown: false }} />
+      <HomepageDetailStack.Screen name="Detail" component={Detail} />
+    </HomepageDetailStack.Navigator>
+  );
+}
+
+const Detail = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button title="Go to Home" onPress={() => navigation.navigate('Search')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
 
 const HomeTab = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="searchTab" component={Search}/>
-      <Tab.Screen name="favoriteTab" component={Favorite}/>
+      <Tab.Screen name="searchTab" component={HomeStackScreen} />
+      <Tab.Screen name="favoriteTab" component={Favorite} />
     </Tab.Navigator>
   )
 }
