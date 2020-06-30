1cb08b86899b64a1889b5c58896a67bbe473a733
thainquet <thainq00@gmail.com>
2020-07-01 01:06:27 +0700

    add favorite posts

--- Screen/appScreen/Favorite.js
+++ Screen/appScreen/Favorite.js
@@ -1,22 +1,103 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Button
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Avatar,
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
+import AsyncStorage from '@react-native-community/async-storage';
+import { posts as baiDang } from '../../favorDummy.js'
 
-const Favorite =({navigation}) => {
-    return(
-      <>
-        <View>
-          <Text>
-            Favorite
-          </Text>
-          
-        </View>
-      </>
-    )
+const getMyStringValue = async () => {
+  try {
+    return await AsyncStorage.getItem('favorite')
+  } catch (e) {
+    // read error
   }
+}
 
-  export default Favorite
+const Favorite = ({ navigation }) => {
+  const [favor, setFavor] = useState(baiDang)
+  // useEffect(() => {
+  //   getMyStringValue().then(res => {
+  //     setFavor(res)
+  //     console.log(res)
+  //   })
+  // })
+  return (
+    <>
+      <View>
+        <ScrollView>
+          <View>
+            {favor.length > 0 ? (favor.map(i => (
+              <TouchableOpacity key={i.id} onPress={() => navigation.push('Detail', { id: i.id })}>
+                <View>
+                  <Text>{i.title}</Text>
+                </View>
+                {/* <View style={{
+                  marginTop: '5%',
+                  width: 500,
+                  flex: 1,
+                  flexDirection: 'row',
+                  justifyContent: 'space-evenly',
+                  alignContent: 'stretch'
+                }}>
+                  <Avatar
+                    style={{
+                      marginLeft: '1%',
+                      flex: 2,
+                      width: 100,
+                      height: 100,
+                    }}
+                    source={{
+                      uri: i.avatar,
+                    }}
+                  />
+                  <View style={{
+                    marginLeft: '5%',
+                    marginRight: '10%',
+                    flex: 6,
+                  }}>
+                    <View>
+                      <Text style={{
+                        flexWrap: "wrap",
+                        fontSize: 18
+                      }}>
+                        {i.title}
+                      </Text>
+                      <Text style={{
+                        flexWrap: "wrap",
+                        fontSize: 16
+                      }}>
+                        Mức lương: {i.salary} vnđ
+                </Text>
+                    </View>
+                    <View style={{ flexDirection: 'row' }}>
+                      {i.timeRequired === 'Partime' ? (<TouchableOpacity style={{ borderWidth: 1, padding: 5, width: 70, marginTop: 10, backgroundColor: '#a78ccc' }}>
+                        <Text>{i.timeRequired}</Text>
+                      </TouchableOpacity>) : (<TouchableOpacity style={{ borderWidth: 1, padding: 5, width: 70, marginTop: 10, backgroundColor: '#5eba7d' }}>
+                        <Text>{i.timeRequired}</Text>
+                      </TouchableOpacity>)}
+                      <TouchableOpacity style={{ borderWidth: 1, borderLeftWidth: 0, padding: 5, width: 90, marginTop: 10, backgroundColor: '#e6e6e6' }}>
+                        <Text>{i.quan}</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View> */}
+              </TouchableOpacity>
+            ))) : <View>
+                <Text>
+                  Khong co muc ua thich
+              </Text>
+              </View>}
+          </View>
+        </ScrollView>
+      </View>
+    </>
+  )
+}
+
+export default Favorite
