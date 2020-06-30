da2315724559580571d43bf13e6af52156b1c58c
thainquet <thainq00@gmail.com>
2020-07-01 01:05:52 +0700

    add favorite data

---
+++ favorDummy.js
@@ -0,0 +1,36 @@
+const posts = [{
+    id: '1',
+    quan: "Thanh Xuân",
+    title: "Công ti A cần tìm người code helloWorld",
+    content: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+     ut labore et dolore magna aliqua. Rhoncus aenean vel elit scelerisque. Habitant morbi tristique
+      senectus et. Malesuada pellentesque elit eget gravida cum sociis natoque penatibus et. Lorem sed 
+      risus ultricies tristique. Eget aliquet nibh praesent tristique magna. Consequat nisl vel pretium
+       lectus quam id leo. Id ornare arcu odio ut. Aliquet bibendum enim facilisis gravida neque 
+       convallis a cras. Mollis aliquam ut porttitor leo a. Montes nascetur ridiculus mus mauris. 
+       Eget mauris pharetra et ultrices neque ornare. Mauris commodo quis imperdiet massa tincidunt 
+       nunc pulvinar sapien et. Sem integer vitae justo eget.`,
+    timeRequired: "Partime",
+    postAuthor: "Tạ Thị B",
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    createAt: "16/04/2020",
+    expiredAt: "31/05/2020",
+    salary: "5.000.000"
+}, {
+    id: '3',
+    quan: "Cầu Giấy",
+    title: "Công ti C cần tìm kế toán",
+    content: `Eget aliquet nibh praesent tristique magna. Consequat nisl vel pretium
+       lectus quam id leo. Id ornare arcu odio ut. Aliquet bibendum enim facilisis gravida neque 
+       convallis a cras. Mollis aliquam ut porttitor leo a. Montes nascetur ridiculus mus mauris. 
+       Eget mauris pharetra et ultrices neque ornare. Mauris commodo quis imperdiet massa tincidunt 
+       nunc pulvinar sapien et. Sem integer vitae justo eget.`,
+    timeRequired: "Partime",
+    postAuthor: "Hồ Văn C",
+    avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
+    createAt: "16/04/2020",
+    expiredAt: "31/05/2020",
+    salary: "5.500.000"
+}
+]
+export { posts }
